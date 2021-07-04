0f5f559184314c8248de0e06ab249e33291991ba
David <zkffhtm6523@naver.com>
2021-07-04 21:35:55 +0900

    메인 페이지 redirect

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("/index.html")
+
+
+@app.route('/about')
+def about():
+    return "about World!"
 
 
 if __name__ == '__main__':
