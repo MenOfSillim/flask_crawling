7238834fbb7704b5312f9cfad94ca110fdc44b60
David <zkffhtm6523@naver.com>
2021-07-05 23:04:09 +0900

    크롤링 확인하기

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
 # Flask 객체 인스턴스 생성
 app = Flask(__name__)
@@ -6,7 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("/index.html")
+    # 엔터치기
+    req = requests.get('https://www.naver.com/')
+
+    # 이런 식으로 HTML에 있는 코드를 다 가져온다
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    myList = []
+
+    for i in soup.select(".theme_cont"):
+        myList.append(i)
+        print(i)
+
+    return render_template("index.html", list=myList)
 
 
 @app.route('/about')
