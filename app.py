9a37571bb7c4f9812aaac5a5d4f85e9bb81fd8d0
David <zkffhtm6523@naver.com>
2021-07-11 23:43:13 +0900

    로그인 구현 및 컨트롤러 구현 완료

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request, flash, redirect
+import requests, logging
 from bs4 import BeautifulSoup
 
 # Flask 객체 인스턴스 생성
 app = Flask(__name__)
+app.secret_key = 'asdf'
 
 
-@app.route('/')
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        userId = request.form.get('userId')
+        userPw = request.form.get('userPw')
+
+        print(f'아이디 :  {userId}, 패스워드 : {userPw}')
+
+        if userId == '' or userPw == '':
+            flash('아이디 혹은 비밀번호를 입력하지 않으셨습니다.')
+            return render_template('login.html')
+        else:
+            flash('로그인 성공')
+            return render_template('login.html')
+    else:
+        return render_template("login.html")
+
+
+@app.route('/', methods=["GET"])
 def index():
+    return "zzz"
+
+
+@app.route('/crawling')
+def crawling():
     # 엔터치기
     req = requests.get('https://www.naver.com/')
 
